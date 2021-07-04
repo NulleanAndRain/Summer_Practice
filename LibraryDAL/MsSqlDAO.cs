70240ff8a4d34605edf82c9b7bc74a539ee46cb4
Nullean <mr000lean@gmail.com>
2021-07-04 18:33:50 +0400

    edited sql dao to use params

--- LibraryDAL/MsSqlDAO.cs
+++ LibraryDAL/MsSqlDAO.cs
@@ -22,9 +22,14 @@ namespace LibraryDAL {
 			}
 		}
 
-		private void execNonQuerry(string querry) {
+		private void execNonQuerry(string querry, List<SqlParameter> _params = null) {
 			void a(SqlConnection conn) {
 				var cmd = new SqlCommand(querry, conn);
+				if (_params != null) {
+					foreach (var param in _params) {
+						cmd.Parameters.Add(param);
+					}
+				}
 				cmd.ExecuteNonQuery();
 			}
 			execWithConnection(a);
@@ -35,15 +40,25 @@ namespace LibraryDAL {
 		#region Books
 
 		public void AddBook(Book book) {
-			var querry = $@"INSERT INTO Books (BookName, Authors, YearOfPublishing)
-							 VALUES ({book.Name}, {book.Authosrs}, {book.YearOfPublishing})";
-			execNonQuerry(querry);
+			var querry = @"INSERT INTO Books (BookName, Authors, YearOfPublishing)
+							 VALUES (@n, @a, @y)";
+			var p = new List<SqlParameter> {
+				new SqlParameter("@n", book.Name),
+				new SqlParameter("@a", book.Authosrs),
+				new SqlParameter("@y", book.YearOfPublishing)
+			};
+			execNonQuerry(querry, p);
 		}
 
-		private List<Book> getBooksWithQuerry(string querry) {
+		private List<Book> getBooksWithQuerry(string querry, List<SqlParameter> _params = null) {
 			List<Book> books = new List<Book>();
 			void a(SqlConnection conn) {
 				var cmd = new SqlCommand(querry, conn);
+				if (_params != null) {
+					foreach (var param in _params) {
+						cmd.Parameters.Add(param);
+					}
+				}
 				using (var reader = cmd.ExecuteReader()) {
 					while (reader.Read()) {
 						var book = new Book();
@@ -66,33 +81,45 @@ namespace LibraryDAL {
 		public List<Book> GetBooksWithAuthors(string[] authors) {
 			if (authors.Length == 0) return null;
 			var querry = new StringBuilder("SELECT * FROM Books WHERE Authors LIKE ");
-			var last = authors.Last();
-			foreach (var author in authors) {
-				querry.Append('%').Append(author).Append('%');
-				if (author != last) {
+			var _params = new List<SqlParameter>();
+			for (int i = 0; i < authors.Length; i++) {
+				var author = authors[i];
+				var paramName = $"@a{i}";
+				querry.Append('%').Append(paramName).Append('%');
+				_params.Add(new SqlParameter(paramName, author));
+				if (i != authors.Length - 1) {
 					querry.Append("AND LIKE ");
 				}
 			}
-			return getBooksWithQuerry(querry.ToString());
+
+			return getBooksWithQuerry(querry.ToString(), _params);
 		}
 
 		public List<Book> GetBooksWithName(string name) {
-			var querry = $"SELECT * FROM Books WHERE BookName LIKE %{name}%";
-			return getBooksWithQuerry(querry.ToString());
+			var querry = "SELECT * FROM Books WHERE BookName LIKE %@n%";
+			var p = new List<SqlParameter> { new SqlParameter("@n", name) };
+			return getBooksWithQuerry(querry, p);
 		}
 
 		public void EditBook(int id, Book newData) {
-			var querry = $@"UPDATE Books
-				SET BookName = {newData.Name}
-				SET Authors = {newData.Authosrs}
-				SET YearOfPublishing = {newData.YearOfPublishing}
-				WHERE ID = {id}";
-			execNonQuerry(querry);
+			var querry = @"UPDATE Books
+				SET BookName = @n
+				SET Authors = @a
+				SET YearOfPublishing = @y
+				WHERE ID = @id";
+			var p = new List<SqlParameter> {
+				new SqlParameter("@n", newData.Name),
+				new SqlParameter("@a", newData.Authosrs),
+				new SqlParameter("@y", newData.YearOfPublishing),
+				new SqlParameter("@id", id)
+			};
+			execNonQuerry(querry, p);
 		}
 
 		public void DeleteBook(int id) {
-			var querry = $@"DELETE FROM Books WHERE ID = {id}";
-			execNonQuerry(querry);
+			var querry = @"DELETE FROM Books WHERE ID = @id";
+			var p = new List<SqlParameter> { new SqlParameter("@id", id) };
+			execNonQuerry(querry, p);
 		}
 
 		#endregion
@@ -101,14 +128,24 @@ namespace LibraryDAL {
 
 		public void CreateUser(User user) {
 			var querry = $@"INSERT INTO Users (Username, PassHash, FirstName, LastName, DateOfBirth)
-							VALUES ({user.Username}, {user.PassHash}, {user.FirstName}, {user.LastName}, {user.DateOfBirth})";
-			execNonQuerry(querry);
+							VALUES (@u, @p, @fn, @ln, @d)";
+			var p = new List<SqlParameter> {
+				new SqlParameter("@u", user.Username),
+				new SqlParameter("@p", user.PassHash),
+				new SqlParameter("@fn", user.FirstName),
+				new SqlParameter("@ln", user.LastName),
+				new SqlParameter("@d", user.DateOfBirth)
+			};
+			execNonQuerry(querry, p);
 		}
 
-		private User selectUser(string querry) {
+		private User selectUser(string querry, List<SqlParameter> _params) {
 			User user = null;
 			void a(SqlConnection conn) {
 				var cmd = new SqlCommand(querry, conn);
+				foreach (var param in _params) {
+					cmd.Parameters.Add(param);
+				}
 				using (var reader = cmd.ExecuteReader()) {
 					if (reader.Read()) {
 						user = new User();
@@ -126,29 +163,40 @@ namespace LibraryDAL {
 		}
 
 		public User GetUserWithName(string username) {
-			var querry = $"SELECT * FROM Users WHERE Username = {username}";
-			return selectUser(querry);
+			var querry = "SELECT * FROM Users WHERE Username = @n";
+			var p = new List<SqlParameter> { new SqlParameter("@n", username) };
+			return selectUser(querry, p);
 		}
 
 		public User GetUserWithId(int id) {
-			var querry = $"SELECT * FROM Users WHERE ID = {id}";
-			return selectUser(querry);
+			var querry = "SELECT * FROM Users WHERE ID = @id";
+			var p = new List<SqlParameter> { new SqlParameter("@id", id) };
+			return selectUser(querry, p);
 		}
-
 
 		public void UpdateUser(int id, User newData) {
 			var querry = $@"UPDATE Users
-				SET Username = {newData.Username}
-				SET PassHash = {newData.PassHash}
-				SET FirstName = {newData.FirstName}
-				SET LastName = {newData.LastName}
-				SET DateOfBirth = {newData.DateOfBirth.ToString("yyyy-MM-dd")}";
+				SET Username = @u
+				SET PassHash = @p
+				SET FirstName = @fn
+				SET LastName = @ln
+				SET DateOfBirth = @d
+				WHERE ID = @id";
+			var p = new List<SqlParameter> {
+				new SqlParameter("@u", newData.Username),
+				new SqlParameter("@p", newData.PassHash),
+				new SqlParameter("@fn", newData.FirstName),
+				new SqlParameter("@ln", newData.LastName),
+				new SqlParameter("@d", newData.DateOfBirth),
+				new SqlParameter("@id", id)
+			};
 			execNonQuerry(querry);
 		}
 
 		public void DeleteUser(int id) {
-			var querry = $"DELETE FROM Users WHERE ID = {id}";
-			execNonQuerry(querry);
+			var querry = "DELETE FROM Users WHERE ID = @id";
+			var p = new List<SqlParameter> { new SqlParameter("@id", id) };
+			execNonQuerry(querry, p);
 		}
 
 		#endregion
