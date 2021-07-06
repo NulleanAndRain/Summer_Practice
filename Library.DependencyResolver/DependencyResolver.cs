1a82478ee66c5ef0346ea98af2b12ed3e03e5f5b
Nullean <mr000lean@gmail.com>
2021-07-06 18:50:29 +0400

    DependencyResolver; hided book template

--- Library.DependencyResolver/DependencyResolver.cs
+++ Library.DependencyResolver/DependencyResolver.cs
@@ -1,10 +1,20 @@
-﻿using System;
+﻿using Library.BLL.Interface;
+using Library.DAL.Interface;
+using LibraryBLL;
+using LibraryDAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Library.DependencyResolver {
-	class DependencyResolver {
+	public class DependencyResolver {
+		public static DependencyResolver Instance { get; } = new DependencyResolver();
+
+		IDAO dao;
+		ILogic logic;
+		public IDAO GetDAO() => dao != null? dao : dao = new MsSqlDAO();
+		public ILogic GetLogicObject() => logic != null? logic : logic = new Logic(GetDAO());
 	}
 }
